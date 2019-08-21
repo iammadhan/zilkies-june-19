e274e76b0b12d7b94587b97731a435d5dd2b8527
Ashwin Arunachalam <aarunachalam@ztech.io>
2019-08-21 18:00:01 +0530

    Minor Changes

--- ashwin/KRA-Angular/src/app/app-routing.module.ts
+++ ashwin/KRA-Angular/src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { OldKraComponent } from './old-kra/old-kra.component';
+import { SetPasswordComponent } from './set-password/set-password.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/login', pathMatch: 'full' },
+  { path: 'login', component: LoginComponent },
+  { path: 'old-kra', component: OldKraComponent },
+  { path: 'set-password', component: SetPasswordComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
